e7ac7047cd587076c559ec53966f55280d0a3230
Richard O'Dwyer <richard@richard.do>
2020-12-18 12:20:34 +0000

    Back to development: 0.2.1

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('HISTORY.md', 'r', encoding='utf-8') as hist_file:
 
 setup(
     name='brunette',
-    version='0.2.0',
+    version='0.2.1.dev0',
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     zip_safe=False,
