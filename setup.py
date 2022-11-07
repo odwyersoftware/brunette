b60d0ef5bc9247c2fc947abacd68d48601a0da22
Vincent <vincentbusiness55@gmail.com>
2022-11-07 18:56:44 +0800

    Fix setup.py description (#27)

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     include_package_data=True,
     zip_safe=False,
-    description='Google/Excel Sheets API Python.',
+    description='A best practice Python code formatter',
     author="O'Dwyer Software",
     author_email='hello@odwyer.software',
     url='https://github.com/ODwyerSoftware/brunette',
